cbaebd5b815f9ee247f8cf8aaba75ab48b357b1b
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-09-06 19:16:29 -0500

    Update PaymentsPage.js

--- frontend/src/pages/PaymentsPage.js
+++ frontend/src/pages/PaymentsPage.js
@@ -1,1 +1,13 @@
+import React from "react";
+import PaymentForm from "../components/PaymentForm";
 
+function PaymentsPage() {
+  return (
+    <div className="page-container">
+      <h2>💳 Payments</h2>
+      <PaymentForm />
+    </div>
+  );
+}
+
+export default PaymentsPage;
